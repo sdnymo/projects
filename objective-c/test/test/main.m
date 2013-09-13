f6ce354c62d0c9d3f8a91b52c9bf96276bcbd167
sdnymo <sdnymo@sdny.local>
2013-09-13 16:18:42 +0100

    1st test commit

---
+++ objective-c/test/test/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  test
+//
+//  Created by Mohanad Hussien on 13/09/2013.
+//  Copyright (c) 2013 sdnymo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
